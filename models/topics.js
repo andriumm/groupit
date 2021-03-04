16120da17502559c11442537d6849ff42995abd7
Beatrice Rossi <rossi.beatrice93@gmail.com>
2021-03-04 17:49:22 +0100

    endpoints

--- models/topics.js
+++ models/topics.js
@@ -1,27 +1,27 @@
 "use strict";
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-	class Topics extends Model {
-		/**
-		 * Helper method for defining associations.
-		 * This method is not a part of Sequelize lifecycle.
-		 * The `models/index` file will call this method automatically.
-		 */
-		static associate(models) {
-			Topics.hasMany(models.Resources);
-			Topics.belongsTo(models.Users);
-		}
-	}
-	Topics.init(
-		{
-			user_id: DataTypes.INTEGER,
-			topic_name: DataTypes.STRING,
-			priority: DataTypes.BOOLEAN,
-		},
-		{
-			sequelize,
-			modelName: "Topics",
-		}
-	);
-	return Topics;
+  class Topics extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Topics.hasMany(models.Resources);
+      Topics.belongsTo(models.Users, { foreignKey: "user_id" });
+    }
+  }
+  Topics.init(
+    {
+      user_id: DataTypes.INTEGER,
+      topic_name: DataTypes.STRING,
+      priority: DataTypes.BOOLEAN,
+    },
+    {
+      sequelize,
+      modelName: "Topics",
+    }
+  );
+  return Topics;
 };
