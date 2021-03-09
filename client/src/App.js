a392c86e0e305d344049b3f6eb6425250fe8143f
Beatrice Rossi <rossi.beatrice93@gmail.com>
2021-03-09 12:07:59 +0100

    git complains

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,11 @@ import "./App.css";
 import Register from "./components/Register";
 
 function App() {
-	return (
-		<div className="App">
-			<Register />
-		</div>
-	);
+  return (
+    <div className="App">
+      <Register />
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/Login.js
@@ -0,0 +1,16 @@
+import React, { useState} from 'react',
+import axios from "axios";
+import { useHistory } from "react-router-dom";
+import "../App.css";
+
+export default function Login() {
+
+
+
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
