8188c3ce6c50a7f644651dc5a031831e17dcc529
icodebyamanda <rasoamanda@gmail.com>
2021-03-09 12:38:33 +0100

    27 dependencies installed

--- client/src/components/InsertTopics.js
+++ client/src/components/InsertTopics.js
@@ -1,1 +1,35 @@
 import React, { useState } from "react";
+
+export default function insertTopics() {
+
+  const [ topic, setTopics ] = useState({
+    topic: "",
+    priority: false,
+  });
+
+  const [ subtopic, setSubtopic ] = useState({
+    subTopic: "",
+});
+
+
+  
+
+  return (
+
+    <div>
+
+      <h1></h1>
+
+
+    </div>
+
+
+
+
+
+  )
+
+
+
+}
+
