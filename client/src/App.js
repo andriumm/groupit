606b85b6f5121ba6ae5a859826e9da5fe14666bb
Beatrice Rossi <rossi.beatrice93@gmail.com>
2021-03-09 12:50:00 +0100

    login component completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
 import "./App.css";
 import Register from "./components/Register";
+import Login from "./components/Login";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Register />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,16 +1,57 @@
-import React, { useState} from 'react',
+import React, { useState } from "react";
 import axios from "axios";
-import { useHistory } from "react-router-dom";
+//import { useHistory } from "react-router-dom";
 import "../App.css";
 
 export default function Login() {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+  //let history = useHistory();
 
+  const handleChange = (e) => {
+    e.persist();
+    setUser((state) => ({ ...state, [e.target.name]: e.target.value }));
+  };
 
+  const login = () => {
+    axios
+      .post("/users/login", user)
+      .then((result) => {
+        //store it locally
+        localStorage.setItem("token", result.data.token);
 
+        //   history.push("/dashboard");
+        console.log(result.data.message, result.data.token);
+      })
+      .catch((error) => console.log(error));
+  };
 
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div>
+      <div>
+        <label htmlFor="email">
+          Email:
+          <input
+            onChange={handleChange}
+            name="email"
+            value={user.email}
+            type="text"
+          />
+        </label>
+        <br />
+        <label htmlFor="password">
+          Password:
+          <input
+            onChange={handleChange}
+            name="password"
+            value={user.password}
+            type="password"
+          />
+        </label>
+        <button onClick={login}>Login</button>
+      </div>
+    </div>
+  );
 }
