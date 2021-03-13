4bcfa51154fb46c3629fa4b09fed75f14967f16c
Beatrice Rossi <rossi.beatrice93@gmail.com>
2021-03-13 13:00:05 +0100

    component started

---
+++ client/src/components/topicsDashboard.js
@@ -0,0 +1,46 @@
+import axios from "axios";
+import React from "react";
+import React, { useState, useEffect } from "react";
+import { useHistory, Link } from "react-router-dom";
+
+export default function topicsDashboard() {
+  const [subtopics, setSubtopics] = useState([]);
+  const [resources, setResources] = useState([]);
+  let history = useHistory();
+
+  //fetch from topics the topics with parents
+  // fetch from resources the resources per subtopic
+  useEffect(() => {
+    getSubtopics();
+    getResources();
+    let token = localStorage.getItem("token");
+    if (!token) {
+      history.push("/login");
+    }
+    console.log(token);
+  }, []);
+
+  const getSubtopics = async () => {
+    try {
+      const subtopic = await axios.get(
+        "/topics",
+        (body = {
+          parent: "",
+        })
+      );
+      setSubtopics(subtopic);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getResources = async () => {
+    try {
+      const resource = await axios.get("/resources");
+      setResources(resource);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return <div></div>;
+}
