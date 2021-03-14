2d2fa1b1ee4f242a8898b5a3394a019f39048c81
Andrea Mestre <amestre89@gmail.com>
2021-03-14 11:50:09 +0100

    profile component added to main

--- client/src/App.js
+++ client/src/App.js
@@ -8,48 +8,54 @@ import AddResource from "./components/AddResource";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
-  let [signedIn, setSignedIn] = useState(localStorage.getItem("token"));
+	let [signedIn, setSignedIn] = useState(localStorage.getItem("token"));
 
-  const handleLogin = () => {
-    setSignedIn(localStorage.getItem("token"));
-    setSignedIn(true);
-  };
+	const handleLogin = () => {
+		setSignedIn(localStorage.getItem("token"));
+		setSignedIn(true);
+	};
 
-  return (
-    <Router>
-      <div className="App">
-        <h1 align="center">GROUP B</h1>
-        <nav align="center">
-          <Link to="/register" className="text-dark ms-3 me-1">
-            Sign Up
-          </Link>
-          <Link to="/login" className="text-dark ms-3 me-1">
-            Sign In
-          </Link>
-          <Link to="/dashboard" className="text-dark ms-3 me-1">
-            Your Dashboard
-          </Link>
-        </nav>
-        <Switch>
-          <Route path="/register">
-            <Register />
-          </Route>
-          <Route path="/login">
-            <Login handleLogin={handleLogin} />
-          </Route>
-          <Route path="/profile">
-            <Profile />
-          </Route>
-          <Route path="/resource">
-            <AddResource />
-          </Route>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+	return (
+		<Router>
+			<div className="App">
+				<h1 align="center">GROUP B</h1>
+				<nav align="center">
+					<Link to="/register" className="text-dark ms-3 me-1">
+						Sign Up
+					</Link>
+					<Link to="/login" className="text-dark ms-3 me-1">
+						Sign In
+					</Link>
+					<Link to="/dashboard" className="text-dark ms-3 me-1">
+						Your Dashboard
+					</Link>
+					<Link to="/myprofile" className="text-dark ms-3 me-1">
+						Your Profile
+					</Link>
+				</nav>
+				<Switch>
+					<Route path="/register">
+						<Register />
+					</Route>
+					<Route path="/login">
+						<Login handleLogin={handleLogin} />
+					</Route>
+					<Route path="/profile">
+						<Profile />
+					</Route>
+					<Route path="/resource">
+						<AddResource />
+					</Route>
+					<Route path="/dashboard">
+						<Dashboard />
+					</Route>
+					<Route path="/myprofile">
+						<Profile />
+					</Route>
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
