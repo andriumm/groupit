a3b22452d0e02bbc53c7656628883e073e0982c8
Beatrice Rossi <rossi.beatrice93@gmail.com>
2021-03-16 18:33:30 +0100

    styled a few pages

--- app.js
+++ app.js
@@ -9,37 +9,37 @@ var usersRouter = require("./routes/users");
 var topicsRouter = require("./routes/topics");
 var resourcesRouter = require("./routes/resources");
 
-
-
 var app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/topics", topicsRouter);
 app.use("/resources", resourcesRouter);
 
-
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-	next(createError(404));
+  next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get("env") === "development" ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-	// render the error page
-	res.status(err.status || 500);
-	res.send("error");
+  // render the error page
+  res.status(err.status || 500);
+  res.send("error");
 });
 
 module.exports = app;
