0746ef3c74449c15cbcd0b636b8feb5e40392fdd
Andrea Mestre <amestre89@gmail.com>
2021-03-17 09:15:33 +0100

    bin icon added

--- client/src/components/ResourcesDashboard.js
+++ client/src/components/ResourcesDashboard.js
@@ -171,7 +171,7 @@ export default function ResourcesDashboard({ subtopic }) {
 											</td>
 											<td scope="col" className="col-1">
 												<select
-													className="custom-select"
+													className="custom-select btn"
 													id="prioritySelect"
 													name="prioritySelect"
 													onChange={(e) => handleFormatChange(e, resource.id)}
@@ -188,7 +188,7 @@ export default function ResourcesDashboard({ subtopic }) {
 											</td>
 											<td scope="col" className="col-1">
 												<select
-													className="custom-select"
+													className="custom-select btn"
 													id="prioritySelect"
 													name="prioritySelect"
 													onChange={(e) => handlePriorityChange(e, resource.id)}
@@ -218,8 +218,25 @@ export default function ResourcesDashboard({ subtopic }) {
 												{resource.created_date.substring(0, 10)}
 											</td>
 											<td scope="col" className="col-1">
-												<button onClick={() => deleteResource(resource.id)}>
-													Delete
+												<button
+													onClick={() => deleteResource(resource.id)}
+													className="btn btn-outline-danger btn-small"
+												>
+													{/* <i class="bi bi-trash"></i> */}
+													<svg
+														xmlns="http://www.w3.org/2000/svg"
+														width="16"
+														height="16"
+														fill="currentColor"
+														class="bi bi-trash"
+														viewBox="0 0 16 16"
+													>
+														<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+														<path
+															fill-rule="evenodd"
+															d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
+														/>
+													</svg>
 												</button>
 											</td>
 										</tr>
