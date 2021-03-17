4d6ddfe1d8593db8f7588f10dbecd1d9b91dd0a7
Andrea Mestre <amestre89@gmail.com>
2021-03-17 16:42:06 +0100

    don't pull this

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,37 +3,37 @@ import { Link, useHistory } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 
 export default function NavBar() {
-  const auth = useAuth();
-  const history = useHistory();
-  const logout = () => {
-    auth.signout(() => history.push("/login"));
-  };
+	const auth = useAuth();
+	const history = useHistory();
+	const logout = () => {
+		auth.signout(() => history.push("/login"));
+	};
 
-  return (
-    <div className="navBar">
-      {!auth.isLoggedIn && (
-        <Link to="/register" className="navBarLink">
-          Register
-        </Link>
-      )}
+	return (
+		<div className="navBar">
+			{!auth.isLoggedIn && (
+				<Link to="/register" className="navBarLink">
+					Register
+				</Link>
+			)}
 
-      {!auth.isLoggedIn && (
-        <Link to="/login" className="navBarLink">
-          Login
-        </Link>
-      )}
+			{!auth.isLoggedIn && (
+				<Link to="/login" className="navBarLink">
+					Login
+				</Link>
+			)}
 
-      {auth.isLoggedIn && (
-        <Link to="/dashboard" className="navBarLink">
-          Dashboard
-        </Link>
-      )}
+			{auth.isLoggedIn && (
+				<Link to="/dashboard" className="navBarLink">
+					Dashboard
+				</Link>
+			)}
 
-      {auth.isLoggedIn && (
-        <button onClick={logout} className="btn logoutButton">
-          Logout
-        </button>
-      )}
-    </div>
-  );
+			{auth.isLoggedIn && (
+				<button onClick={logout} className="btn logoutButton">
+					Logout
+				</button>
+			)}
+		</div>
+	);
 }
