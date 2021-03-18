b12a9258092751203ce9079aad683dbb7f3e0a11
icodebyamanda <rasoamanda@gmail.com>
2021-03-18 11:07:56 +0100

    #77 styling changes before merging Andrea's

--- client/src/components/TopicPage.js
+++ client/src/components/TopicPage.js
@@ -77,22 +77,24 @@ export default function TopicPage({ onUpdateSubtopic }) {
 
   return (
     <div>
-      <h2 className="text-uppercase">{topic.topic_name}</h2>
+      <h2 className="text-uppercase d-flex">{topic.topic_name}</h2>
       {/* What could be added: link on the name of the subtopic to redirect to the Resources Dashboard */}
       <div className="dashboardBox" >
         {subtopics.map((subtopic) => (
           <div className="perTopicBoxDiv" key={subtopic.id}>
             {/* <div className="mainBox" className="perTopicBoxDiv" key={subtopic.id}> */}
             {/* <div className="mainBoxHeader" id="perTopicBox"> */}
-            <div id="perTopicBox">
-              <h5
-                className="titlePerTopic text-uppercase" 
+            <div id="perTopicBox d-flex">
+
+            <h5
+                className="titlePerTopic text-uppercase d-flex" 
                 onClick={() => goToResourcesDashboard(subtopic)}
               >
                 {subtopic.topic_name}
               </h5>
+            
               <button
-                className="deleteButtonPerTopic btn"
+                className="deleteButtonPerTopic btn d-flex"
                 onClick={() => deleteSubtopic(subtopic.id)}
               >
                 {/* <i class=“bi bi-trash”></i> */}
@@ -101,16 +103,18 @@ export default function TopicPage({ onUpdateSubtopic }) {
                   width="16"
                   height="16"
                   fill="currentColor"
-                  class="bi bi-trash"
+                  className="bi bi-trash d-flex"
                   viewBox="0 0 16 16"
                 >
                   <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
                   <path
-                    fill-rule="evenodd"
+                    fillrule="evenodd"
                     d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
                   />
                 </svg>
+
               </button>
+
             </div>
             {subtopic.Resources.map((resource) => (
               <div key={resource.id} className="subBox">
